71e9b3a5ad79439c21f3fd23cbb7a5a3e4f30cef
Mickaël Pham <inbox@mickael.dev>
2024-01-13 12:57:21 +0100

    Basic directory with logger

---
+++ src/logger.ts
@@ -0,0 +1,13 @@
+import winston from 'winston'
+
+export const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL ?? 'info',
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+    }),
+  ],
+})
